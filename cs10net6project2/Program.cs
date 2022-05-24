c895fab0d58f49e507c88c8af4bf647bd6925585
AyaneSHOJI <shojiayane@gmail.com>
2022-05-25 00:17:09 +0200

    Add project files.

---
+++ cs10net6project2/Program.cs
@@ -0,0 +1,12 @@
+﻿using static System.Console;
+
+//********Chapter3 ************
+
+//postfix vs prefix 
+int a = 3;
+int b = a++;
+WriteLine($"a is {a}, b is {b}");// 4, 3
+
+int c = 4;
+int d = ++c;
+WriteLine($"c is {c}, d is {d}");//5, 5 
