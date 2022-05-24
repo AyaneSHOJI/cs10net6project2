a75f8cdb59594b82cafccd1bd14206aac890501a
AyaneSHOJI <shojiayane@gmail.com>
2022-05-25 00:39:59 +0200

    added Note

--- cs10net6project2/Program.cs
+++ cs10net6project2/Program.cs
@@ -9,4 +9,17 @@ WriteLine($"a is {a}, b is {b}");// 4, 3
 
 int c = 4;
 int d = ++c;
-WriteLine($"c is {c}, d is {d}");//5, 5 
+WriteLine($"c is {c}, d is {d}");//5, 5
+
+//bitwise and binary shift operators
+int a1 = 10;//00001010
+int b1 = 6;//00000110
+WriteLine($"a1 & b1 = { a1 & b1 }");//2 bit
+WriteLine($"a1 | b1 = { a1 | b1 }");//8, 4  and 2 bit
+WriteLine($"a1 ^ b1 = { a1 ^ b1 }");//8 and 4 bit
+
+static string ToBinaryString(int value)
+{
+    return Convert.ToString(value, toBase: 2).PadLeft(8, '0');
+}
+WriteLine($"b1 = {ToBinaryString(b1)}");
